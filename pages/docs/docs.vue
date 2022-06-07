9dfa8a79e66bda8055c8273d5bd7e75106c96166
goodListener <good_listener@naver.com>
2022-06-07 13:28:44 +0900

    nuxt first commit

---
+++ pages/docs/docs.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-row>
+    <el-col>
+      test
+    </el-col>
+    <el-col>
+      123
+    </el-col>
+  </el-row>
+</template>
+<script setup lang="ts">
+
+</script>
---
+++ pages/secondPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>secondPage
+    <el-button>Button</el-button>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
